852fd0c24f6a7cfd25afc1e02ad43ee75ecc8d52
Ivan Yardley <ivanyardley@me.com>
2026-07-21 15:43:56 +0100

    fix(seo): exclude opengraph-image + twitter-image from auth middleware

--- middleware.ts
+++ middleware.ts
@@ -6,5 +6,5 @@ const { auth } = NextAuth(authConfig)
 export default auth
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon\\.ico|sitemap\\.xml|robots\\.txt|llms\\.txt|login|marketing|forgot-password|reset-password|accept-invite|accept-dpa|accept-terms|fonts|.*\\.png|.*\\.jpg|.*\\.jpeg|.*\\.svg|.*\\.ico|.*\\.webp|.*\\.gif).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon\\.ico|sitemap\\.xml|robots\\.txt|llms\\.txt|opengraph-image|twitter-image|login|marketing|forgot-password|reset-password|accept-invite|accept-dpa|accept-terms|fonts|.*\\.png|.*\\.jpg|.*\\.jpeg|.*\\.svg|.*\\.ico|.*\\.webp|.*\\.gif).*)'],
 }
